2216c551324f586917b45afb7e7f25aecdffe98f
Albers <Vieraa680@gmail.com>
2025-07-17 13:56:48 -0300

    Initial GDExtension project structure

---
+++ src/register_types.h
@@ -0,0 +1,9 @@
+#ifndef GEOLOCATION_REGISTER_TYPES_H
+#define GEOLOCATION_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+void initialize_geolocation_module(godot::ModuleInitializationLevel p_level);
+void uninitialize_geolocation_module(godot::ModuleInitializationLevel p_level);
+
+#endif
